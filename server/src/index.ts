27a9c411221719966d27e417fef22cc5a94ec375
liamross <liamross.code@gmail.com>
2019-09-21 16:12:11 -0700

    - Created scraper - Logging and error handling improved in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import {AmazonItem} from '../../shared/types';
+import {scrapeByAsin} from './scraper';
 
 const PORT = 3001;
 
@@ -36,35 +37,80 @@ const mockItems = new Map<string, AmazonItem>([
 ]);
 
 const app = express();
+app.use(cors());
 
 /* --- All items --- */
 
-app.use(cors());
+/** Get all stored items. */
+app.get('/item', (_req, res, next) => {
+  console.log('GET /item');
 
-app.get('/item', (_req, res) => res.send(Array.from(mockItems.values())));
+  try {
+    // TODO: Get items from DB.
+    const returnVal = Array.from(mockItems.values());
+    console.log(returnVal);
+    res.send(returnVal);
+  } catch (error) {
+    next(error);
+  }
+});
 
-app.delete('/item', (_req, res) => {
-  // TODO: delete.
-  res.status(204).send();
+/** Delete all stored items. */
+app.delete('/item', (_req, res, next) => {
+  console.log('DELETE /item');
+
+  try {
+    // TODO: Delete items in DB.
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 });
 
 /* --- Specific item --- */
 
-app.get('/item/:asin', (req, res) => {
-  const item = mockItems.get(req.params.asin);
-  if (item) {
-    res.send(item);
-  } else {
-    // TODO: crawl the item.
+/**
+ * Get an item by asin.
+ * 1. If it exists in DB, return it.
+ * 2. If not, scrape amazon site for data and store, then return.
+ */
+app.get('/item/:asin', async (req, res, next) => {
+  const asin = req.params.asin;
+  console.log(`GET /item/${asin}`);
+
+  try {
+    // TODO: Get items from DB, or null/undefined if none.
+    const item = mockItems.get(asin);
+    if (item) {
+      console.log('Item is already saved, returning.');
+      console.log(item);
+      res.send(item);
+    } else {
+      const amazonItem = await scrapeByAsin(asin);
+      console.log('Item scraped, returning.');
+      // TODO: store item, then return with stored date.
+      console.log(amazonItem);
+      res.send(amazonItem);
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
-app.delete('/item/:asin', (req, res) => {
-  if (mockItems.has(req.params.asin)) {
-    // TODO: delete.
-    res.status(204).send();
-  } else {
-    res.status(404).send('Item does not exist.');
+/** Delete an item by asin. */
+app.delete('/item/:asin', (req, res, next) => {
+  const asin = req.params.asin;
+  console.log(`DELETE /item/${asin}`);
+
+  try {
+    if (mockItems.has(asin)) {
+      // TODO: delete.
+      res.status(204).send();
+    } else {
+      throw new Error('Item does not exist.');
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
---
+++ server/src/scraper.ts
@@ -0,0 +1,74 @@
+import puppeteer from 'puppeteer';
+import {AmazonItem, Rank} from '../../shared/types';
+
+const INFORMATION_TABLE_SELECTOR = 'div.pdTab';
+
+// https://github.com/GoogleChrome/puppeteer/issues/3051
+
+export async function scrapeByAsin(asin: string): Promise<Omit<AmazonItem, 'storedDate'>> {
+  const browser = await puppeteer.launch();
+
+  try {
+    const page = await browser.newPage();
+    await page.goto(`https://www.amazon.com/dp/${asin}`);
+
+    // Wait for information table to load before parsing information.
+    await page.waitForSelector(INFORMATION_TABLE_SELECTOR);
+
+    const name = await page.evaluate(() => {
+      const element = document.getElementById('productTitle') as HTMLSpanElement;
+      return element.innerText;
+    });
+
+    const imgUrl = await page.evaluate(() => {
+      const element = document.getElementById('imgTagWrapperId') as HTMLDivElement;
+      const img = element.firstElementChild as HTMLImageElement;
+      return img.src;
+    });
+
+    const category = await page.evaluate(() => {
+      const element = document.getElementById('wayfinding-breadcrumbs_feature_div') as HTMLDivElement;
+      const ul = element.firstElementChild as HTMLUListElement;
+      const allLIChildren: HTMLLIElement[] = Array.prototype.slice.call(ul.children);
+      const textLIChildren = allLIChildren.reduce(
+        (liArray, li, index) => (index % 2 == 0 ? [...liArray, li] : liArray),
+        [] as HTMLLIElement[],
+      );
+      return textLIChildren.map(li => {
+        const span = li.firstElementChild as HTMLSpanElement;
+        const a = span.firstElementChild as HTMLAnchorElement;
+        return a.innerText;
+      });
+    });
+
+    const rank = await page.evaluate(() => {
+      const element = document.getElementById('SalesRank') as HTMLTableRowElement;
+      const valueTD = element.querySelectorAll('td')[1];
+      const splitRanks = valueTD.innerText.split('\n');
+      return splitRanks.map<Rank>(splitRank => {
+        const cleanRank = splitRank.split(/\s\(/)[0];
+        console.log({cleanRank});
+        const [rankString, category] = cleanRank.split(/\sin\s/);
+        console.log({rankString, category});
+        return {rank: Number(rankString.substring(1)), category};
+      });
+    });
+
+    const dimensions = await page.evaluate(() => {
+      const element = document.querySelectorAll('tr.size-weight')[1] as HTMLTableRowElement;
+      const valueTD = element.querySelectorAll('td')[1];
+      const dimensionString = valueTD.innerText;
+      const [x, z, yAndUnits] = dimensionString.split(/\sx\s/);
+      const [y, units] = yAndUnits.split(/\s/);
+      return {x: Number(x), z: Number(z), y: Number(y), units};
+    });
+
+    await browser.close();
+
+    return {asin, name, imgUrl, category, rank, dimensions};
+  } catch (error) {
+    console.error(error);
+    await browser.close();
+    throw new Error();
+  }
+}
